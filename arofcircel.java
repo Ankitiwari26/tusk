8912963612116c3d6992a6e47c474d16226fd467
Ankitiwari26 <151852629+Ankitiwari26@users.noreply.github.com>
2023-11-23 19:43:23 +0530

    Add files via upload

---
+++ arofcircel.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class Area
+{
+public static void main(String args[])
+{
+int radius=21;
+
+float area=(radius*radius)*22/7f;
+System.out.print("Area of the circle"+area);
+}
+}
